d49111725c398e5d70f1bb736f672ee753a029bb
Bruce Merry <bmerry@ska.ac.za>
2017-03-03 09:09:01 +0200

    Fixups for restart module
    Based on some code review feedback.

--- katsdpservices/restart.py
+++ katsdpservices/restart.py
@@ -3,6 +3,7 @@ from __future__ import print_function, division, absolute_import
 import sys
 import os.path
 import signal
+import logging
 
 
 def setup_restart(signum=signal.SIGHUP, restart_callback=None):
@@ -20,19 +21,22 @@ def setup_restart(signum=signal.SIGHUP, restart_callback=None):
     Returns
     -------
     restart : callable
-        Function that does 
+        Function that restarts the process
     """
     def restart():
-        logger = logging.getLogger(__file__)
         if restart_callback is not None:
             restart_callback()
-        logger.info("Restarting")
         # Ensure any logging gets properly flushed
         sys.stdout.flush()
         sys.stderr.flush()
         os.execlp(sys.executable, args[0], *args)
+    def restart_handler(signum, frame):
+        logger = logging.getLogger(__file__)
+        logger.warn("Received signal %d, restarting", signum)
+        restart()
     # Latch the absolute path now, in case something changes directory later
     # and makes a relative path invalid.
     args = list(sys.argv)
     args[0] = os.path.abspath(args[0])
-    signal.signal(signal.SIGHUP, lambda signum, frame: restart())
+    signal.signal(signal.SIGHUP, restart_handler)
+    return restart
