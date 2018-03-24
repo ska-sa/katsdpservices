5e13d9f70d4864d97ddf47e8ad0e05e611dc114a
Bruce Merry <bmerry@gmail.com>
2018-03-24 16:04:15 +0200

    Add tests for GELF logging

--- katsdpservices/test/test_logging.py
+++ katsdpservices/test/test_logging.py
@@ -3,12 +3,18 @@
 from __future__ import print_function, division, absolute_import
 import time
 import logging
+import socket
 import os
 import re
 import signal
-import mock
+import json
+import zlib
+from contextlib import closing
+
 import unittest2 as unittest
+import mock
 import six
+
 import katsdpservices
 
 
@@ -25,6 +31,13 @@ class TestLogging(unittest.TestCase):
         self.addCleanup(patcher.stop)
         return mock_obj
 
+    def _wipe_dummy_logger(self):
+        logger = logging.getLogger('katsdpservices.test.dummy')
+        for handler in list(logger.handlers):
+            logger.removeHandler(handler)
+        for filter in list(logger.filters):
+            logger.removeFilter(filter)
+
     def setUp(self):
         # Grab the stderr written by the logger
         self.stderr = self._create_patch('sys.stderr', new_callable=six.StringIO)
@@ -32,6 +45,7 @@ class TestLogging(unittest.TestCase):
         # that.
         self.logger = self._create_patch(
             'logging.root', logging.getLogger('katsdpservices.test.dummy'))
+        self.addCleanup(self._wipe_dummy_logger)
         # Wipe out existing environment, so that we can poke KATSDP_LOG_*
         self.environ = self._create_patch_dict(os.environ, clear=True)
         # Override time so that we can compare against a known value
@@ -82,6 +96,53 @@ class TestLogging(unittest.TestCase):
                 "2017-03-02T14:02:03.125Z - test_logging.py:\\d+ - WARNING - warning message\n"
                 "with\n"
                 "newlines\n\\Z", re.M))
+
+    def _test_gelf(self, localname, extra):
+        self.maxDiff = 4096   # Diff tends to be too big to display otherwise
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        with closing(sock):
+            sock.bind(('127.0.0.1', 0))
+            port = sock.getsockname()[1]
+            os.environ['KATSDP_LOG_GELF_ADDRESS'] = '127.0.0.1:{}'.format(port)
+            if localname:
+                os.environ['KATSDP_LOG_GELF_LOCALNAME'] = 'myhost'
+            if extra:
+                os.environ['KATSDP_LOG_GELF_EXTRA'] = '{"hello": "world", "number": 3}'
+            katsdpservices.setup_logging()
+            logging.info('info message')
+            raw = sock.recv(4096)
+            raw = zlib.decompress(raw)
+            print(repr(raw))
+        data = json.loads(raw.decode('utf-8'))
+        # This dictionary may need to be updated depending on the implementation
+        expected = {
+            u"timestamp": self.time.return_value,
+            u"version": u"1.0",
+            u"short_message": u"info message",
+            u"full_message": u"info message",
+            u"facility": u"katsdpservices.test.dummy",
+            u"file": mock.ANY,
+            u"line": mock.ANY,
+            u"level": 6,
+            u"host": u"myhost" if localname else mock.ANY,
+            u"_function": u"_test_gelf",
+            u"_thread_name": u"MainThread",
+            u"_process_name": u"MainProcess",
+            u"_pid": mock.ANY,
+        }
+        if extra:
+            expected[u"_hello"] = u"world"
+            expected[u"_number"] = 3
+        # It's only set on Python 3
+        if u"_stack_info" in data:
+            expected[u"_stack_info"] = None
+        self.assertEqual(data, expected)
+
+    def test_gelf_basic(self):
+        self._test_gelf(False, False)
+
+    def test_gelf_options(self):
+        self._test_gelf(True, True)
 
     def test_toggle_debug(self):
         self.assertEqual(logging.INFO, logging.root.level)
