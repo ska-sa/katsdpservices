58826f4573be33392eefbbe4c7eff958959a897e
Bruce Merry <bmerry@ska.ac.za>
2020-05-08 11:23:56 +0200

    Use README.md as long_description in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
+
+import os
 from setuptools import setup, find_packages
 
 tests_require = ['nose', 'katsdptelstate', 'aiomonitor']
+long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 setup(
     name="katsdpservices",
-    description=("common code used by services that make up the telescope system, "
+    description=("Common code used by services that make up the MeerKAT Science Data Processor, "
                  "such as a common logging setup"),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="MeerKAT SDP team",
     author_email="sdpdev+katsdpservices@ska.ac.za",
+    url='https://github.com/ska-sa/katsdpservices',
     packages=find_packages(),
     setup_requires=["katversion"],
     install_requires=["netifaces", "pygelf>=0.3.6"],
